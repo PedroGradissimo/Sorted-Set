1cae0986e099c98c1c44548da79840347c62bf24
PedroGradissimo <gradispe@gmail.com>
2021-08-12 16:05:12 +0100

    Added Containment tests

--- sorted_frozen_set.py
+++ sorted_frozen_set.py
@@ -1,4 +1,12 @@
 class SortedFrozenSet:
 
     def __init__(self, item=None):
-        pass
+        self._items = (
+            list(item) if (item is not None)
+            else list()
+        )
+
+    # This special method accepts a single argument which is the item
+    # for which to test for containment, and returns a boolean
+    def __contains__(self, item):
+        return item in self._items
--- test_sorted_frozen_set.py
+++ test_sorted_frozen_set.py
@@ -21,5 +21,25 @@ class TestConstruction(unittest.TestCase):
         s = SortedFrozenSet()
 
 
+class TestContainerProtocol(unittest.TestCase):
+
+    # Method used to prepare the test context. In this example is used
+    # to attach text fixtures to the test instance self.
+    def setUp(self):
+        self.s = SortedFrozenSet([6, 7, 3, 9])
+
+    def test_positive_contain(self):
+        self.assertTrue(6 in self.s)
+
+    def test_negative_contain(self):
+        self.assertFalse(2 in self.s)
+
+    def test_positive_not_contained(self):
+        self.assertTrue(5 not in self.s)
+
+    def test_negative_not_contained(self):
+        self.assertFalse(9 not in self.s)
+
+
 if __name__ == "__main__":
     unittest.main()
