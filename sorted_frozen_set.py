d5887aa2080b8cce894f393b5368779d001e90c0
PedroGradissimo <gradispe@gmail.com>
2021-08-12 16:29:51 +0100

    Added Size protocol tests

--- sorted_frozen_set.py
+++ sorted_frozen_set.py
@@ -2,11 +2,15 @@ class SortedFrozenSet:
 
     def __init__(self, item=None):
         self._items = (
-            list(item) if (item is not None)
-            else list()
+            set(item) if (item is not None)
+            else set()
         )
 
     # This special method accepts a single argument which is the item
     # for which to test for containment, and returns a boolean
     def __contains__(self, item):
         return item in self._items
+
+    # Size protocol
+    def __len__(self):
+        return len(self._items)
--- test_sorted_frozen_set.py
+++ test_sorted_frozen_set.py
@@ -23,6 +23,7 @@ class TestConstruction(unittest.TestCase):
 
 class TestContainerProtocol(unittest.TestCase):
 
+    # CONTAIN PROTOCOL
     # Method used to prepare the test context. In this example is used
     # to attach text fixtures to the test instance self.
     def setUp(self):
@@ -39,6 +40,32 @@ class TestContainerProtocol(unittest.TestCase):
 
     def test_negative_not_contained(self):
         self.assertFalse(9 not in self.s)
+
+
+# SIZE PROTOCOL
+class TestSizedProtocol(unittest.TestCase):
+
+    def test_empty_with_default(self):
+        s = SortedFrozenSet()
+        self.assertEqual(len(s), 0)
+
+    def test_empty(self):
+        s = SortedFrozenSet([])
+        self.assertEqual(len(s), 0)
+
+    def test_one(self):
+        s = SortedFrozenSet([42])
+        self.assertEqual(len(s), 1)
+
+    def test_ten(self):
+        s = SortedFrozenSet(range(10))
+        self.assertEqual(len(s), 10)
+
+    # In the SortedFrozenSet constructor the set method
+    # eliminates the duplicates
+    def test_with_duplicates(self):
+        s = SortedFrozenSet([5, 5, 5])
+        self.assertEqual(len(s), 1)
 
 
 if __name__ == "__main__":
